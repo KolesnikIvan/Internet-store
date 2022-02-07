4ce377d6dc54266c890a13d4f8c62fb00c561eae
KolesnikIvan <86139761+KolesnikIvan@users.noreply.github.com>
2022-02-07 17:43:03 +0500

    Add files via upload

---
+++ mainapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', views.products, name='all'),  # 'products'),
+    path('<int:category_id>', views.category, name='category'),
+    path('product/<int:product_id>', views.product, name='product'),
+    # path('/', mainapp.products, name='products'),
+    # path('/<int:pk>', mainapp.category, name='category'),
+]
+# originally without slash
